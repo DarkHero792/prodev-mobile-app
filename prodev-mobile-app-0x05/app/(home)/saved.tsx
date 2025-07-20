6c1ddcf4301bf6c5b88c5d55d0b63a22c8c117c2
Abdelhamid_Marmar <abdelhamidmarmar4@gmail.com>
2025-07-20 18:49:38 +0100

    Add files via upload

---
+++ prodev-mobile-app-0x05/app/(home)/saved.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function ScreenName() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Screen Name</Text>
+    </View>
+  );
+}
+
+
